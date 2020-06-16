9523e00874eed40ecdfcbd6f992bd2f81ab64ace
jang <97jjy97@naver.com>
2020-06-16 19:01:27 +0900

    메인 아이템 추가

--- view/pc/main/main.php
+++ view/pc/main/main.php
@@ -91,7 +91,77 @@
 <section class="area-list m60t">
         <div class="list-wrap">
         <ul class="counselor-list" id="item-list">
-            
+            <!-- 아이템 리스트 -->
+            <li class="counselor m20r m30b">
+                <a href="javascript:;">
+                    <div class="area-status" style="background: url('<?php echo site_url()?>assets/image/cache/back.png') no-repeat; background-size: cover;">
+                        <ul class="type">
+                            <li class="method" style="background: #540191">타로</li>
+                            <li class="tag"># 연애상담</li><li class="tag"># 방향제시</li>
+                        </ul>
+                        <img src="<?php echo site_url()?>assets/image/cache/image.png" alt="photo" class="counselor_photo">
+                        <div class="on"><span>상담중</span></div>
+                    </div>
+                </a>
+                <div class="about-counselor">
+                    <div class="wrap">
+                        <div class="name-counselor">
+                            <a href="javascript:;">
+                                <h3 class="m10b">
+                                    <div class="main-new-label">
+                                        <span class="reserve-label">예약</span>
+                                    </div>
+                                    앨리스 <span class="highlight" style="margin-left:3px; margin-right:0px;">365</span>번
+                                    <div class="notice_status m5l">
+                                        <span class="profile-notice"></span>
+                                    </div>
+                                </h3>
+                            </a>
+                            <p>속마음 잘 맞추는 커플궁합 전문 타로마스터 </p>
+                        </div>
+                        <ul class="points-counselor">
+                            <li class="new-3month none-line">
+                                <span>최근 3개월</span>
+                            </li>
+                            <li class="star">
+                                <span class="weight">5.0</span>
+                            </li>
+                            <li>
+                                <span>후기</span><span class="weight">999+</span>
+                            </li>
+                        </ul>
+                        <ul class="recommend-counselor">
+                            <li>
+                                <a href="javascript:;">
+                                    <span class="words">언니~~언니가 왜 오늘 내일 쉴거같다 했는지 알겠어용~.</span>
+                                    <span class="name">비올레타</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">
+                                    <span class="words">안 쓴 후기 생각나서 부랴부랴 쓰러 왔어요ㅎㅎㅎ 잘 지.</span>
+                                    <span class="name">ㄹ연</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">
+                                    <span class="words">솔직하게 얘기해주셔서 더 도움이 된것같아요~ 그사람 성.</span>
+                                    <span class="name">evian</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="numbers-counselor">
+                        <ul>
+                            <li><span>코인상담</span> : 070-4769-7000</li>
+                            <li><span>060상담 </span> : 060-800-7788</li>
+                        </ul>
+                        <div class="calling">
+                            전화연결 후 <span class="highlight">365</span>번
+                        </div>
+                    </div>
+                </div>
+            </li>
         </ul>
         <div class="loading-image" id="loading_sign" style="display: none;"><img src="<?php echo IMAGEPATH?>icon/loading-animation.gif" alt=""></div>
         <a href="javascript:;" style="" class="btn-more m20t" id="next-block"><span>상담사 리스트 더보기</span></a>
